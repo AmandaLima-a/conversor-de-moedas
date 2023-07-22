6a28c302c688cfd40ba79ef1f1e79fb873f9beaa
AmandaLima-a <amandasuelemjl@gmail.com>
2023-07-21 21:18:06 -0300

    fim projeto

--- scripts.js
+++ scripts.js
@@ -1,27 +1,69 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert =  document.querySelector(".currency-value-to-convert") //valor real
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor real
     const currencyValueConverted = document.querySelector(".currency-value") // Outras moedas
 
-    const dolarToday = 5.2 /* Valor ficticio */
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    const dolarToday = 4.8 
+    const euroToday = 5.34
+    const libraToday = 6.15
 
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', { 
-        style: 'currency', 
-        currency: 'BRL' 
+
+    if (currencySelect.value == 'dolar') {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(inputCurrencyValue / dolarToday);
+    }
+
+    if (currencySelect.value == 'euro') {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR'
+        }).format(inputCurrencyValue / euroToday);
+    }
+
+    if (currencySelect.value == 'libra') {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat('en-GB', {
+            style: 'currency',
+            currency: 'GBP'
+        }).format(inputCurrencyValue / libraToday);
+    }
+
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
     }).format(inputCurrencyValue);
-    
-    currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', { 
-        style: 'currency', 
-        currency: 'USD' 
-    }).format(convertedValue);
-
-    console.log(convertedValue)
 
 }
 
+function changeCurrency() {
+    const currencyName = document.querySelector('#currency-name')
+    const currencyImg = document.querySelector('.currency-img')
 
-convertButton.addEventListener("click", convertValues) /* Toda vez que o Button for clicado vai chamar essa function */ 
+    if (currencySelect.value == 'dolar') {
+        currencyName.innerHTML = 'Dólar Americano'
+        currencyImg.src = './assets/estados-unidos.png'
+    }
+
+    if (currencySelect.value == 'euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImg.src = './assets/euro.png'
+    }
+
+    if (currencySelect.value == 'libra') {
+        currencyName.innerHTML = 'Libra'
+        currencyImg.src = './assets/libra 1.png'
+    }
+
+
+    convertValues()
+}
+
+currencySelect.addEventListener('change', changeCurrency)
+convertButton.addEventListener('click', convertValues) /* Toda vez que o Button for clicado vai chamar essa function */ 
